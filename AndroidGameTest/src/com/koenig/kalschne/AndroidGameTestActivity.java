1be86b22d549bc78d865a36afac723f7b17bc4cc
Daniel Koenig <koenig1986@googlemail.com>
2012-01-16 20:03:34 +0100

    initial

---
+++ AndroidGameTest/src/com/koenig/kalschne/AndroidGameTestActivity.java
@@ -0,0 +1,13 @@
+package com.koenig.kalschne;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidGameTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
