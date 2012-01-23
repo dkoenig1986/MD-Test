82bad272533861f6815c49d0390790c2a577f715
Daniel Koenig <koenig1986@googlemail.com>
2012-01-23 22:03:12 +0100

    Texture

--- AndroidOpenGlTutorial/src/com/koenig/AndroidOpenGlTutorialActivity.java
+++ AndroidOpenGlTutorial/src/com/koenig/AndroidOpenGlTutorialActivity.java
@@ -1,11 +1,17 @@
 package com.koenig;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.util.Random;
 
 import javax.microedition.khronos.opengles.GL10;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLUtils;
+import android.util.Log;
 
 import com.koenig.framework.Game;
 import com.koenig.framework.Screen;
@@ -20,7 +26,8 @@ public class AndroidOpenGlTutorialActivity extends GLGame {
 	class TriangleScreen extends Screen {
 		GLGraphics glGraphics;
 		FloatBuffer vertices;
-		int VERTEX_SIZE = (2 + 4) * 4;
+		int VERTEX_SIZE = (2 + 2) * 4;
+		int textureId;
 
 		public TriangleScreen(Game game) {
 			super(game);
@@ -29,8 +36,30 @@ public class AndroidOpenGlTutorialActivity extends GLGame {
 			ByteBuffer byteBuffer = ByteBuffer.allocateDirect(3 * VERTEX_SIZE);
 			byteBuffer.order(ByteOrder.nativeOrder());
 			vertices = byteBuffer.asFloatBuffer();
-			vertices.put(new float[] { 0.0f, 0.0f, 1, 0, 0, 1, 319.0f, 0.0f, 0, 1, 0, 1, 160.0f, 479.0f, 0, 0, 1, 1 });
+			vertices.put(new float[] { 0.0f, 0.0f, 0.0f, 1.0f, 319.0f, 0.0f, 1.0f, 1.0f, 160.0f, 479.0f, 0.5f, 0.0f });
 			vertices.flip();
+
+			textureId = loadTexture("test.bmp");
+		}
+
+		public int loadTexture(String fileName) {
+			try {
+				Bitmap bitmap = BitmapFactory.decodeStream(game.getFileIO().readAsset(fileName));
+				GL10 gl = glGraphics.getGl();
+				int textureIds[] = new int[1];
+				gl.glGenTextures(1, textureIds, 0);
+				int textureId = textureIds[0];
+				gl.glBindTexture(GL10.GL_TEXTURE_2D, textureId);
+				GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
+				gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
+				gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_NEAREST);
+				gl.glBindTexture(GL10.GL_TEXTURE_2D, 0);
+				bitmap.recycle();
+				return textureId;
+			} catch (IOException e) {
+				Log.d("TexturedTriangleTest", "couldn't load asset 'bobrgb888.png'!");
+				throw new RuntimeException("couldn't load asset '" + fileName + "'");
+			}
 		}
 
 		@Override
@@ -46,9 +75,8 @@ public class AndroidOpenGlTutorialActivity extends GLGame {
 			vertices.position(0);
 			gl.glVertexPointer(2, GL10.GL_FLOAT, VERTEX_SIZE, vertices);
 			vertices.position(2);
-			gl.glColorPointer(4, GL10.GL_FLOAT, VERTEX_SIZE, vertices);
+			gl.glTexCoordPointer(2, GL10.GL_FLOAT, VERTEX_SIZE, vertices);
 			gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3);
-
 		}
 
 		@Override
@@ -71,8 +99,10 @@ public class AndroidOpenGlTutorialActivity extends GLGame {
 			gl.glLoadIdentity();
 			gl.glOrthof(0, 320, 0, 480, 1, -1);
 			gl.glColor4f(1, 0, 0, 1);
+			gl.glEnable(GL10.GL_TEXTURE_2D);
+			gl.glBindTexture(GL10.GL_TEXTURE_2D, textureId);
 			gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-			gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+			gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
 
 		}
 
